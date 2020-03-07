b7397e293beb067afe40ed536e2b141dfbf5f133
Quyen Le <heyquyenle@gmail.com>
2020-03-07 12:48:34 -0800

    setting up firebase

---
+++ logic.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDWfCPY_fMb_BCEn6paqsJwobVDbo22_B4",
+    authDomain: "trainscheduler-4280e.firebaseapp.com",
+    databaseURL: "https://trainscheduler-4280e.firebaseio.com",
+    projectId: "trainscheduler-4280e",
+    storageBucket: "trainscheduler-4280e.appspot.com",
+    messagingSenderId: "246519424219",
+    appId: "1:246519424219:web:a8d36948e4bcad1a1b58fe"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database=firebase.database();
+  
